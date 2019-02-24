85e94d8ac5fdd2f2b1896a1e08e68d4d322e0a1a
McNuggets7778 <mockvich88@gmail.com>
2019-02-24 16:31:09 +0300

    24.02.19

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+ $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 2500, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
  // grecaptcha.ready(function() {
  //      grecaptcha.execute('reCAPTCHA_site_key', {action: 'homepage'}).then(function(token) {
  //         ...
@@ -7,4 +20,3 @@
  $(document).ready(function(){
       $('.slider').bxSlider();
     });
-
